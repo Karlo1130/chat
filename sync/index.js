a1bfd9ef639e31637ac569ed1825192f54cac97f
Doncan00 <90412543+Doncan00@users.noreply.github.com>
2025-06-09 23:50:17 -0700

    Los mensajes se muestran en la pantalla

--- sync/index.js
+++ sync/index.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const cors = require('cors');
-const SynchronizationService = require('./services/synchronization');
-const dbClient = require('../shared/config/db');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+const SynchronizationService = require("./services/synchronization");
+const dbClient = require("../shared/config/db");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  }
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
 const PORT = process.env.PORT || 4000;
@@ -20,25 +20,23 @@ app.use(cors());
 app.use(express.json());
 
 // WebSocket: manejar unión a salas
-io.on('connection', (socket) => {
-  console.log('Cliente conectado');
+io.on("connection", (socket) => {
+  console.log("Cliente conectado");
 
-  socket.on('joinRoom', (room) => {
+  socket.on("joinRoom", (room) => {
     socket.join(room);
     console.log(`Cliente se unió a la sala: ${room}`);
   });
 
-  socket.on('disconnect', () => {
-    console.log('Cliente desconectado');
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado");
   });
 });
 
-
-SynchronizationService.on('newMessage', (message) => {
-  io.to(message.room).emit('newMessage', message);
+SynchronizationService.on("newMessage", (message) => {
+  io.to(message.room).emit("newMessage", message);
 });
 
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Sync/WebSocket server running on port ${PORT}`);
 });
-
